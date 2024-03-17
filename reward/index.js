3cc80b113007dec0e02ea0c9462efa6356de2cf1
Y-Fr <2602977906@qq.com>
2024-03-17 17:07:31 +0800

    Site updated 03-2024年3月17日 17:07:28

--- reward/index.js
+++ reward/index.js
@@ -85,7 +85,7 @@ while(not my_input == "exit"):
 AV.init({
   appId: "qwUdSwIm8YI1ilHhjiqIkGap-gzGzoHsz",
   appKey: "ToJcW1bIMIeMc0ZQ4fYzoMIa",
-  serverURL: 'leancloud.cn',
+  serverURL: 'https://leancloud.cn',
 });
 console.log(window.AV)
 // 声明 class
